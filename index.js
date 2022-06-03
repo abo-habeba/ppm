d63866fe981c00859a120cc7af2ffda297f45530
abo-habeba <94226077+abo-habeba@users.noreply.github.com>
2022-06-03 22:16:05 +0200

    1111

---
+++ index.js
@@ -0,0 +1,8 @@
+let jton     = document.getElementById("ton");
+let jtime    = document.getElementById("time");
+let jppm     = document.getElementById("ppm");
+let joutNum  = document.getElementById("outNum");
+
+function  total(){
+    joutNum.value = jton.value * jtime.value * jppm.value / 1000;
+};
