435bf27b3b9da772fb2735e3e89f7f798795027e
abo-habeba <94226077+abo-habeba@users.noreply.github.com>
2022-06-07 04:29:30 +0200

    Update js.js

--- js.js
+++ js.js
@@ -6,17 +6,8 @@ function  total(){
     joutNum.value = jton.value * jtime.value * jppm.value / 1000;
 };
 
-let  jfoot = document.getElementById("myFoot");
-
-jfoot.innerText = 'By Hesham Mohamed © 2022';
-
-
 
 let myFooter =document.createElement('footer');
-let myP   = document.createElement('p');
-
-myP.innerText = `By Hesham Mohamed © 2022`;
-
-document.body.appendChild(myFooter).appendChild(myP);
-
-myFooter.style.cssText = 'background-color: dimgray;position:fixed;bottom:0px; width: 100%;height:50px;margin:0px;padding:0px; text-align:center;color:white;display: flex;align-items:center;justify-content: center';
+document.body.appendChild(myFooter);
+myFooter.innerText = 'By Hesham Mohamed © 2022';
+myFooter.style.cssText = 'background-color: dimgray; position:fixed; bottom:0px; width: 100%; height:50px; margin:0px; padding:0px; text-align:center; color:white;display: flex;align-items:center;justify-content: center';
